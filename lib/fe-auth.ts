03212f74318df2312e20dbd55babf3eb27149f3a
AlesBacul <alesbacul@gmail.com>
2026-02-05 19:04:11 +0100

    refactor login function to improve data validation and error handling

--- lib/fe-auth.ts
+++ lib/fe-auth.ts
@@ -1,33 +1,51 @@
 'use server'
-
+import { z } from 'zod'
 import { redirect } from 'next/navigation'
 import { getPayload } from 'payload'
 import config from '@payload-config'
 const payload = await getPayload({ config })
 import { cookies } from 'next/headers'
+import { password } from 'payload/shared'
 
 const tokenCookieName = 'payload-token'
 
 export async function login(formData: FormData) {
   'use server'
 
-  const rawFormData = {
-    email: formData.get('email'),
-    password: formData.get('password'),
+  const rawData = {
+    email: String(formData.get('email')),
+    password: String(formData.get('password')),
   }
 
+  const schema = z.object({
+    email: z.email(),
+    password: z.string(),
+  })
+
   let result
+
+  const validatedFields = schema.safeParse({
+    email: rawData.email,
+    password: rawData.password,
+  })
+
+  if (!validatedFields.success) {
+    console.log('error validating')
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
   try {
     result = await payload.login({
       collection: 'users', // required
       data: {
         // required
-        email: rawFormData.email,
-        password: rawFormData.password,
+        email: rawData.email,
+        password: rawData.password,
       },
       //req: req, // optional, pass a Request object to be provided to all hooks
       depth: 2,
-      locale: 'en',
+      locale: 'cs',
       //fallbackLocale: false,
       overrideAccess: false,
       showHiddenFields: true,
@@ -48,5 +66,5 @@ export async function login(formData: FormData) {
 export async function logout() {
   const cookieStore = await cookies()
   cookieStore.delete(tokenCookieName)
-  redirect('login')
+  redirect('/login')
 }
