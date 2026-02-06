45ad467fc1a42f5508cab9da133151889adb3ea9
AlesBacul <alesbacul@gmail.com>
2026-02-06 21:27:14 +0100

    add dynamic page for displaying individual posts based on slug

---
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { headers as nextHeaders } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { getPayload } from 'payload'
+import config from '@/payload.config'
+import { getIdFromSlug } from 'lib/string-methods'
+import { getPostById } from 'lib/PostService'
+interface Props {
+  params: Promise<{ slug: string }>
+}
+export default async function Page({ params }: Props) {
+  const headers = await nextHeaders()
+
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { user } = await payload.auth({ headers, canSetHeaders: false })
+
+  if (user == null) {
+    redirect('/login')
+  }
+
+  const { slug } = await params
+
+  const entityId = getIdFromSlug(slug)
+
+  if (entityId == null) {
+    return <>Neplatný odkaz</>
+  } else {
+    const post = await getPostById(entityId)
+    return <>My post {JSON.stringify(post)}</>
+  }
+}
