2089b37a3ffd0a6985a0530705883688521c6003
AlesBacul <alesbacul@gmail.com>
2026-02-06 21:53:50 +0100

    refactor PostGrid component to enhance layout and integrate RichTextDefault for content display

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -1,6 +1,10 @@
 import { PaginatedDocs } from 'payload'
 import { Post } from '@/payload-types'
 import Link from 'next/link'
+import { RichText } from '@payloadcms/richtext-lexical/react'
+import { DefaultTypedEditorState } from '@payloadcms/richtext-lexical'
+import RichTextDefault from './RichTextDefault'
+import LinkButton from './LinkButton'
 interface Props {
   paginatedDocs: PaginatedDocs<Post>
 }
@@ -11,15 +15,19 @@ export default function PostGrid(props: Props) {
   const docs = paginatedDocs.docs
 
   return (
-    <>
+    <div className="flex flex-col gap-2">
       {docs.map((doc) => {
+        const data = doc.content as DefaultTypedEditorState
         return (
-          <div key={doc.id} className="border-2 border-amber-500">
+          <div key={doc.id} className="border-2 rounded border-blue-500 p-2 md:p-4">
             <h2>{doc.title}</h2>
-            <Link href={'#'}>Přečíst</Link>
+            <RichTextDefault data={data} />
+            <div>
+              <LinkButton href={`/posts/${doc.id}-${doc.slug}`} label={'Detail'} />
+            </div>
           </div>
         )
       })}
-    </>
+    </div>
   )
 }
