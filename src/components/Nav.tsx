10abae292125bc01e2f65e4d66be4f3c21338a65
AlesBacul <alesbacul@gmail.com>
2026-02-08 22:00:37 +0100

    refactor Nav component to streamline class assignment for navigation items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,17 +4,16 @@ import { LogOutIcon, HouseIcon } from 'lucide-react'
 
 import Link from 'next/link'
 export default function Nav() {
+  const navItemClass =
+    'h-full w-full flex py-4 px-2 gap-x-2  hover:bg-slate-200 transition-colors justify-center rounded-4xl'
   return (
-    <div className=" bg-slate-100 border-b border-slate-200">
+    <div className=" m-2  rounded-4xl bg-slate-100  border-b border-slate-200">
       <div className="flex flex-col text-center">
         <header className="text-2xl p-2">Výlety</header>
         <nav>
           <ul className="flex md:justify-center w-full">
             <li className="flex grow md:grow-0 ">
-              <Link
-                className="h-full w-full flex py-4 px-2 gap-x-2   hover:bg-slate-200 transition-colors justify-center"
-                href={'/'}
-              >
+              <Link className={navItemClass} href={'/'}>
                 <span className="text-slate-500">
                   <HouseIcon />
                 </span>
@@ -22,10 +21,7 @@ export default function Nav() {
               </Link>
             </li>
             <li className="flex grow md:grow-0">
-              <button
-                className="h-full w-full flex py-4 px-2 gap-x-2   hover:bg-slate-200 cursor-pointer transition-colors justify-center"
-                onClick={logout}
-              >
+              <button className={navItemClass.concat(' ', 'cursor-pointer')} onClick={logout}>
                 <span className="text-slate-500">
                   <LogOutIcon />
                 </span>
