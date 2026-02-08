df647755beb07c1bd2f3df94002d866fcd891304
AlesBacul <alesbacul@gmail.com>
2026-02-08 11:02:14 +0100

    refactor Nav component to improve structure and enhance navigation links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,21 @@
 import { logout } from 'lib/fe-auth'
 import Button from './Button'
+import Link from 'next/link'
 export default function Nav() {
   return (
-    <div>
-      <Button onClick={logout} label="Odhlásit se" variant="primary" />
-    </div>
+    <nav className=" bg-gray-100">
+      <div className="flex flex-col text-center">
+        <div className="text-2xl p-2">Výlety</div>
+        <Link className="p-2 hover:bg-gray-200" href={'/'}>
+          Domů
+        </Link>
+        <button className="p-2 hover:bg-gray-200 cursor-pointer" onClick={logout}>
+          Odhlásit se
+        </button>
+        {/* <div>
+          <Button onClick={logout} label="Odhlásit se" variant="primary" />
+        </div> */}
+      </div>
+    </nav>
   )
 }
