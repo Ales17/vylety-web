2f4fd81b23f16a82201d63150fe95f4e3997c9a6
AlesBacul <alesbacul@gmail.com>
2026-02-09 15:24:11 +0100

    refactor getPosts function to include isDraft parameter for filtering posts

--- lib/PostService.ts
+++ lib/PostService.ts
@@ -1,17 +1,26 @@
 import { getPayload } from 'payload'
 import config from '@/payload.config'
-import { Post } from '@/payload-types'
 
 const payloadConfig = await config
 const payload = await getPayload({ config: payloadConfig })
 
-export async function getPosts({ limit = 10, page }: { limit?: number; page: number }) {
+export async function getPosts({
+  limit = 10,
+  page,
+  isDraft = false,
+}: {
+  limit?: number
+  page: number
+  isDraft?: boolean
+}) {
   const result = await payload.find({
     collection: 'posts', // required
     page: page,
     limit: limit,
     pagination: true, // If you want to disable pagination count, etc.
-    where: {}, // pass a `where` query here
+    where: {
+      isDraft: { equals: isDraft },
+    }, // pass a `where` query here
     sort: '-createdAt',
   })
   return result
