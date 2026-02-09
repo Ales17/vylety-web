c23c6907668c5153ef8e0e670516b51363f31cd1
AlesBacul <alesbacul@gmail.com>
2026-02-09 11:28:04 +0100

    refactor LinkButton component to include optional icon prop and enhance styling for better user experience

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -3,12 +3,16 @@ import type { Url } from 'next/dist/shared/lib/router/router'
 interface Props {
   href: Url
   label: string
+  icon?: React.ReactNode
 }
 
-export default function LinkButton({ href, label }: Props) {
+export default function LinkButton({ href, label, icon }: Props) {
   return (
-    <Link href={href} className="inline-block p-2 rounded text-white bg-blue-500">
-      {label}
+    <Link
+      href={href}
+      className="inline-flex gap-x-2 hover:gap-x-2.5 py-4 px-2 bg-slate-900 text-white rounded-2xl font-medium transition-all hover:bg-slate-800 active:scale-95 shadow-sm"
+    >
+      <span>{label}</span> {icon && <span>{icon}</span>}
     </Link>
   )
 }
