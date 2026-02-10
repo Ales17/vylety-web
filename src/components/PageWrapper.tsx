e90cc5d2a5e864e05426eaf72e47822385cfeaf7
AlesBacul <alesbacul@gmail.com>
2026-02-10 22:06:35 +0100

    style(PageWrapper): add optional date prop to display publication date

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,11 +1,13 @@
 interface Props {
   children: React.ReactNode
   pageName: string
+  date?: string
 }
-export default function PageWrapper({ children, pageName }: Props) {
+export default function PageWrapper({ children, pageName, date }: Props) {
   return (
     <div className=" border rounded-3xl bg-white border-slate-200 p-2 md:p-4">
       <h1 className="text-4xl mb-2">{pageName}</h1>
+      {date && <p>{date}</p>}
       {children}
     </div>
   )
