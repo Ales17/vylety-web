714faeff8a67dd86747d86b93fff24f47270e20e
AlesBacul <alesbacul@gmail.com>
2026-02-13 00:26:42 +0100

    feat(auth): integrate password login functionality alongside magic link login

--- lib/fe-auth.ts
+++ lib/fe-auth.ts
@@ -11,22 +11,75 @@ const tokenCookieName = 'payload-token'
 const webUrl = process.env.WEBSITE_URL || 'URL'
 const adminEmail = process.env.ADMIN_EMAIL
 
-export async function magicLinkLogin(initialState: any, formData: FormData) {
+export async function passwordLogin(initialState: any, formData: FormData) {
   const rawData = {
     email: String(formData.get('email')),
-    // password: String(formData.get('password')),
+    password: String(formData.get('password')),
   }
 
   const schema = z.object({
     email: z.email(),
-    // password: z.string(),
+    password: z.string(),
+  })
+
+  let result
+
+  const validatedFields = schema.safeParse({
+    email: rawData.email,
+    password: rawData.password,
+  })
+
+  if (!validatedFields.success) {
+    console.log('error validating')
+    return {
+      message: 'Validate errors!', //errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+  try {
+    result = await payload.login({
+      collection: 'users', // required
+      data: {
+        // required
+        email: rawData.email,
+        password: rawData.password,
+      },
+      //req: req, // optional, pass a Request object to be provided to all hooks
+      depth: 2,
+      locale: 'cs',
+      //fallbackLocale: false,
+      overrideAccess: false,
+      showHiddenFields: true,
+    })
+    //console.log(result)
+  } catch (err: any) {
+    console.error(err)
+  }
+
+  if (result && result.token) {
+    const cookieStore = await cookies()
+    cookieStore.set(tokenCookieName, result?.token, {
+      path: '/',
+      httpOnly: true,
+    })
+    redirect('/')
+  }
+
+  return { message: 'Přihlášení se nezdařilo' }
+}
+
+export async function magicLinkLogin(initialState: any, formData: FormData) {
+  const rawData = {
+    email: String(formData.get('email')),
+  }
+
+  const schema = z.object({
+    email: z.email(),
   })
 
   let result
   let token
   const validatedFields = schema.safeParse({
     email: rawData.email,
-    // password: rawData.password,
   })
 
   if (!validatedFields.success) {
@@ -59,19 +112,18 @@ export async function magicLinkLogin(initialState: any, formData: FormData) {
   }
 
   if (!token) {
-    // Same message for non-existens users and errors - security standard 
+    // Same message for non-existens users and errors - security standard
     return { message: 'Pokud u nás máte účet, e-mail dorazí během chvíle.' }
   }
 
-
   try {
-    const prettyUrl = formatUrl(webUrl) || webUrl; // Full URL fallback if formatting fails 
+    const prettyUrl = formatUrl(webUrl) || webUrl // Full URL fallback if formatting fails
 
     await payload.sendEmail({
       to: validatedFields.data.email,
       subject: `Přihlášení na ${prettyUrl}`,
-      text: `Ahoj, \npro přihlášení na web ${prettyUrl} použij následující odkaz:\n${webUrl}/login/${token}`,
-      html: `Ahoj,<br>pro přihlášení na web ${prettyUrl} použij odkaz: <a href="${webUrl}/login/${token}">${prettyUrl}</a>`,
+      text: `Pro přihlášení na web ${prettyUrl} použijte následující odkaz:\n${webUrl}/login/${token}\nPokud by nefungoval, zkopírujte ho do prohlížeče`,
+      html: `Pro přihlášení na web ${prettyUrl} použijte následující odkaz:<br> <a href="${webUrl}/login/${token}">${prettyUrl}</a><br>Pokud by nefungoval, zkopírujte ho do prohlížeče`,
     })
 
     return { message: 'Zkontroluj si e-mailovou schránku.' }
@@ -79,7 +131,6 @@ export async function magicLinkLogin(initialState: any, formData: FormData) {
     console.error('Email failed', emailErr)
     return { message: 'Chyba při odesílání e-mailu.' }
   }
-
 }
 
 export async function logout() {
--- src/app/(fe)/(auth)/login/page.tsx
+++ src/app/(fe)/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { magicLinkLogin } from 'lib/fe-auth'
+import { magicLinkLogin, passwordLogin } from 'lib/fe-auth'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
 import { useActionState } from 'react'
@@ -7,8 +7,11 @@ import { useActionState } from 'react'
 const initialState = {
   message: '',
 }
+
+const actionToUse = process.env.NEXT_PUBLIC_AUTH_METHOD === 'magic' ? magicLinkLogin : passwordLogin
+
 export default function Login() {
-  const [state, formAction, pending] = useActionState(magicLinkLogin, initialState)
+  const [state, formAction, pending] = useActionState(actionToUse, initialState)
 
   return (
     <div>
@@ -20,7 +23,9 @@ export default function Login() {
           placeholder="email@email.cz"
           label="E-mail"
         />
-        {/* <Input type="password" name="password" id="password" placeholder="******" label="Heslo" /> */}
+        {process.env.NEXT_PUBLIC_AUTH_METHOD === 'password' && (
+          <Input type="password" name="password" id="password" placeholder="******" label="Heslo" />
+        )}
         <Button label={'Přihlásit se'} variant="primary" />
         <p aria-live="polite">{state?.message}</p>
       </form>
