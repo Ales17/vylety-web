14bec95ead54615f046f00da2a16a85af8d16dd3
AlesBacul <alesbacul@gmail.com>
2026-02-12 12:21:53 +0100

    feat(auth): enhance magic link login by adding admin email restriction and improving email subject formatting

--- lib/fe-auth.ts
+++ lib/fe-auth.ts
@@ -8,7 +8,9 @@ import { cookies } from 'next/headers'
 import { formatUrl } from './string-methods'
 
 const tokenCookieName = 'payload-token'
-const webUrl = process.env.WEBSITE_URL
+const webUrl = process.env.WEBSITE_URL || 'URL'
+const adminEmail = process.env.ADMIN_EMAIL
+
 export async function login(initialState: any, formData: FormData) {
   'use server'
 
@@ -23,7 +25,7 @@ export async function login(initialState: any, formData: FormData) {
   })
 
   let result
-
+  let token
   const validatedFields = schema.safeParse({
     email: rawData.email,
     // password: rawData.password,
@@ -35,45 +37,44 @@ export async function login(initialState: any, formData: FormData) {
       message: 'E-mail není ve správném formátu!', //errors: validatedFields.error.flatten().fieldErrors,
     }
   }
+  // Prevent password change for admin, so he can
+  if (validatedFields.data.email == adminEmail) {
+    return {
+      message: 'Magic link není povolen.',
+    }
+  }
+
   try {
     const token = await payload.forgotPassword({
       collection: 'users', // required
       data: {
-        // required
         email: validatedFields.data.email,
       },
       disableEmail: true,
       //req: req, // pass a Request object to be provided to all hooks
     })
-    console.log(token)
-
-    const loginUrl = `${webUrl}/login/${token}`
-    if (webUrl) {
-      const prettyUrl = formatUrl(webUrl)
-      const email = await payload.sendEmail({
-        to: validatedFields.data.email,
-        subject: 'Přihlášení na vylety.bacul.cz',
-        text: `Ahoj, \npro přihlášení na web ${prettyUrl} použij následující odkaz:\n${loginUrl}\nKdyby nefungoval, zkopíruj ho do prohlížeče ručně.`,
-        html: `Ahoj,<br>pro přihlášení na web ${prettyUrl} použij následující odkaz:<br><a href="${webUrl}/login/${token}">${webUrl}/login/${token}</a><br>Kdyby nefungoval, zkopíruj ho do prohlížeče ručně.`,
-      })
-    }
+    console.log('TOKEN', token)
 
     //console.log(result)
   } catch (err: any) {
-    console.error(err)
+    console.error('ERR', err)
   }
+  const prettyUrl = formatUrl(webUrl)
 
-  /* 
-  if (result && result.token) {
-    const cookieStore = await cookies()
-    cookieStore.set(tokenCookieName, result?.token, {
-      path: '/',
-      httpOnly: true,
-    })
-    redirect('/')
+  if (token == null) {
+    return { message: 'Nemáš oprávnění k přístupu.' }
+  } else {
+    if (prettyUrl) {
+      const loginUrl = `${webUrl}/login/${token}`
+      const email = await payload.sendEmail({
+        to: validatedFields.data.email,
+        subject: `Přihlášení na ${prettyUrl}`,
+        text: `Ahoj, \npro přihlášení na web ${prettyUrl} použij následující odkaz:\n${loginUrl}\nKdyby nefungoval, zkopíruj ho do prohlížeče ručně.`,
+        html: `Ahoj,<br>pro přihlášení na web ${prettyUrl} použij následující odkaz:<br><a href="${webUrl}/login/${token}">${webUrl}/login/${token}</a><br>Kdyby nefungoval, zkopíruj ho do prohlížeče ručně.`,
+      })
+      return { message: 'Zkontroluj si e-mailovou schránku.' }
+    }
   }
- */
-  return { message: 'Zkontroluj si e-mailovou schránku.' }
 }
 
 export async function logout() {
