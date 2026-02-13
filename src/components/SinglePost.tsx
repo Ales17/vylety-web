673b2319568712869581980a6c21d9562bfe615e
AlesBacul <alesbacul@gmail.com>
2026-02-13 15:22:42 +0100

    refactor: conditionally render TripInfoItem list in SinglePost component

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -11,26 +11,32 @@ interface Props {
 export default function SinglePost({ post }: Props) {
   return (
     <div className="flex flex-col gap-y-4">
-      <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2">
-        <TripInfoItem
-          title="Datum akce"
-          icon={<Calendar1Icon />}
-          label={formatDate(post.eventDate)}
-        />
-
-        <TripInfoItem
-          title="Délka trasy"
-          icon={<FootprintsIcon />}
-          label={`${post.tripLength} km`}
-        />
-
-        <TripInfoItem
-          icon={<MapIcon />}
-          label={'Mapa trasy'}
-          href={post.mapsLink}
-          title="Otevřít trasu"
-        />
-      </ul>
+      {(post.eventDate || post.tripLength || post.mapsLink) && (
+        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2">
+          {post.eventDate && (
+            <TripInfoItem
+              title="Datum akce"
+              icon={<Calendar1Icon />}
+              label={formatDate(post.eventDate)}
+            />
+          )}
+          {post.tripLength && (
+            <TripInfoItem
+              title="Délka trasy"
+              icon={<FootprintsIcon />}
+              label={`${post.tripLength} km`}
+            />
+          )}
+          {post.mapsLink && (
+            <TripInfoItem
+              icon={<MapIcon />}
+              label={'Mapa trasy'}
+              href={post.mapsLink}
+              title="Otevřít trasu"
+            />
+          )}
+        </ul>
+      )}
       {post.content && <RichTextDefault data={post.content} />}
       {post.gallery && <Gallery images={post.gallery} />}
     </div>
